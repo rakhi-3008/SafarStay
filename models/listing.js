5edcee06d836c7200f6eb7772444ea599fff7e61
Rakhi <rakhipanwar819@gmail.com>
2026-04-21 22:39:07 +0530

    form validation and error handling

--- models/listing.js
+++ models/listing.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: String,
-    image: {
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
     filename: String,
-    url: String
-    },
-    price: Number,
-    location: String,
-    country: String
+    url: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
