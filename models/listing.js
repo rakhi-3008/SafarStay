3d9f5799f3e0c94462aa459917f2db37a105a522
Rakhi <rakhipanwar819@gmail.com>
2026-04-26 00:52:54 +0530

    reviews creation, validation, deleting

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,5 @@
-const { string } = require("joi");
 const mongoose = require("mongoose");
+const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -25,6 +25,18 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    },
+  ]
+});
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+      if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+      }
 });
 
 module.exports = mongoose.model("Listing", listingSchema);
